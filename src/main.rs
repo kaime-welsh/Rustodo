2173106f97cf579a21a8ebf101ab1ccd583686be
sketchyscribe <kaime.r.welsh@gmail.com>
2024-03-09 01:37:58 -0800

    Changed command parser
    - Removed argh as dependecy
    - Added "edit" command
    - Added "help" command

--- src/main.rs
+++ src/main.rs
@@ -1,132 +1,42 @@
-use std::fs;
+mod todo;
 
-use argh::FromArgs;
-use comfy_table::modifiers::UTF8_SOLID_INNER_BORDERS;
-use comfy_table::{presets::UTF8_FULL, Cell, Color, ContentArrangement};
-use serde_derive::{Deserialize, Serialize};
+fn help() {
+    println!(
+        "\
+A simple todo management app in the terminal.
 
-// TODO:
-// Add arguments and parsing
+Usage: todo [SUBCOMMAND] [VALUE]
 
-#[derive(FromArgs)]
-/// create and manage todo lists from the terminal
-struct Args {
-    #[argh(option, short = 'a')]
-    /// add new todo
-    add: Option<String>,
-
-    #[argh(option, short = 'f')]
-    /// mark a todo as complete
-    finish: Option<usize>,
-
-    #[argh(option, short = 'r')]
-    /// remove an item from the list
-    remove: Option<usize>,
-
-    #[argh(switch, short = 'c')]
-    /// clean finished todo's from list
-    clean: bool,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct TodoItem {
-    description: String,
-    status: bool,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct TodoList {
-    items: Vec<TodoItem>,
-}
-
-impl TodoList {
-    fn new() -> Self {
-        if !std::path::Path::new("list.json").exists() {
-            fs::write("list.json", b"").expect("failed to create new file");
-        }
-        Self { items: Vec::new() }
-    }
-
-    fn add(&mut self, desc: &str) {
-        self.items.push(TodoItem {
-            description: desc.to_string(),
-            status: false,
-        });
-    }
-
-    fn finish(&mut self, index: usize) {
-        self.items[index].status = true;
-    }
-
-    fn remove(&mut self, index: usize) {
-        self.items.remove(index);
-    }
-
-    fn clean(&mut self) {
-        self.items.retain_mut(|i| !i.status);
-    }
-
-    fn write(&self) {
-        let j = serde_json::to_string_pretty(self).unwrap();
-        fs::write("list.json", j.as_bytes()).unwrap();
-    }
-
-    fn read(&mut self) {
-        let data = fs::read_to_string("list.json").unwrap();
-        if !data.is_empty() {
-            self.items = serde_json::from_str::<TodoList>(&data).unwrap().items;
-        }
-    }
-
-    fn print(&self) {
-        let mut table = comfy_table::Table::new();
-        table
-            .load_preset(UTF8_FULL)
-            .apply_modifier(UTF8_SOLID_INNER_BORDERS)
-            .set_content_arrangement(ContentArrangement::Dynamic);
-
-        table.set_header(vec!["#", "☐", "Description"]);
-
-        for (index, item) in self.items.iter().enumerate() {
-            let idx = Cell::new(index.to_string()).fg(Color::Yellow);
-            let description = match item.status {
-                true => Cell::new(item.description.to_string())
-                    .add_attribute(comfy_table::Attribute::CrossedOut),
-                false => Cell::new(item.description.to_string()),
-            };
-            let status = match item.status {
-                true => Cell::new("☑").fg(Color::Green),
-                false => Cell::new("☐"),
-            };
-
-            table.add_row(vec![idx, status, description]);
-        }
-
-        if self.items.is_empty() {
-            table.add_row(vec!["", "", "No more items to do!"]);
-        }
-
-        println!("{table}");
-    }
+Subcommands:
+    add,    -a, --add    '<DESCRIPTION>'       add a new todo item
+    edit,   -e, --edit   <ID> '<DESCRIPTION>'  edit an existing todo item
+    finish, -f, --finish <ID>                  mark an item as finished
+    remove, -r, --remove <ID>                  remove an item
+    clean,  -c, --clean                        clean the list of finished items
+    help,   -h, --help                         print help"
+    );
 }
 
 fn main() {
-    let mut list = TodoList::new();
+    let mut list = todo::TodoList::new();
     list.read();
 
-    let args: Args = argh::from_env();
+    let args: Vec<String> = std::env::args().collect();
 
-    if args.clean {
-        list.clean();
-    } else if args.add.is_some() {
-        list.add(&args.add.unwrap());
-    } else if args.finish.is_some() {
-        list.finish(args.finish.unwrap());
-    } else if args.remove.is_some() {
-        list.remove(args.remove.unwrap());
+    if args.len() <= 1 {
+        list.print();
+    } else {
+        match args[1].as_str() {
+            "add" | "-a" | "--add" => list.add(&args[2].as_str()),
+            "finish" | "-f" | "--finish" => list.finish(args[2].parse().unwrap()),
+            "edit" | "-e" | "--edit" => list.edit(args[2].parse().unwrap(), args[3].as_str()),
+            "remove" | "-r" | "--remove" => list.remove(args[2].parse().unwrap()),
+            "clean" | "-c" | "--clean" => list.clean(),
+            "help" | "-h" | "--help" => help(),
+            _ => {
+                println!("\x1b[31;1mInvalid command!\x1b[0m");
+                help();
+            }
+        }
     }
-
-    list.write();
-    // println!("{}", serde_json::to_string_pretty(&list).unwrap());
-    list.print();
 }
---
+++ src/todo.rs
@@ -0,0 +1,100 @@
+use comfy_table::modifiers::UTF8_SOLID_INNER_BORDERS;
+use comfy_table::{presets::UTF8_FULL, Cell, Color, ContentArrangement};
+use serde_derive::{Deserialize, Serialize};
+use std::fs;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TodoItem {
+    description: String,
+    status: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TodoList {
+    items: Vec<TodoItem>,
+}
+
+impl TodoList {
+    pub fn new() -> Self {
+        if !std::path::Path::new("list.json").exists() {
+            fs::write("list.json", b"").expect("failed to create new file");
+        }
+        Self { items: Vec::new() }
+    }
+
+    pub fn add(&mut self, desc: &str) {
+        self.items.push(TodoItem {
+            description: desc.to_string(),
+            status: false,
+        });
+        self.write();
+        self.print();
+    }
+
+    pub fn edit(&mut self, index: usize, desc: &str) {
+        self.items[index].description = desc.to_string();
+        self.write();
+        self.print();
+    }
+
+    pub fn finish(&mut self, index: usize) {
+        self.items[index].status = true;
+        self.write();
+        self.print();
+    }
+
+    pub fn remove(&mut self, index: usize) {
+        self.items.remove(index);
+        self.write();
+        self.print();
+    }
+
+    pub fn clean(&mut self) {
+        self.items.retain_mut(|i| !i.status);
+        self.write();
+        self.print();
+    }
+
+    pub fn write(&self) {
+        let j = serde_json::to_string_pretty(self).unwrap();
+        fs::write("list.json", j.as_bytes()).unwrap();
+    }
+
+    pub fn read(&mut self) {
+        let data = fs::read_to_string("list.json").unwrap();
+        if !data.is_empty() {
+            self.items = serde_json::from_str::<TodoList>(&data).unwrap().items;
+        }
+    }
+
+    pub fn print(&self) {
+        let mut table = comfy_table::Table::new();
+        table
+            .load_preset(UTF8_FULL)
+            .apply_modifier(UTF8_SOLID_INNER_BORDERS)
+            .set_content_arrangement(ContentArrangement::Dynamic);
+
+        table.set_header(vec!["#", "☐", "Description"]);
+
+        for (index, item) in self.items.iter().enumerate() {
+            let idx = Cell::new(index.to_string()).fg(Color::Yellow);
+            let description = match item.status {
+                true => Cell::new(item.description.to_string())
+                    .add_attribute(comfy_table::Attribute::CrossedOut),
+                false => Cell::new(item.description.to_string()),
+            };
+            let status = match item.status {
+                true => Cell::new("☑").fg(Color::Green),
+                false => Cell::new("☐"),
+            };
+
+            table.add_row(vec![idx, status, description]);
+        }
+
+        if self.items.is_empty() {
+            table.add_row(vec!["", "", "No more items to do!"]);
+        }
+
+        println!("{table}");
+    }
+}
